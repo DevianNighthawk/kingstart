bc407ba5140ca3b55ffa7b9d38471fe7b576957b
DevianNightgale <deviacorps@gmail.com>
2020-08-12 15:25:08 -0700

    added couple of tests to read_data function

--- pndora/pndora.py
+++ pndora/pndora.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 class EmptyStringError(Exception):
 	def __init__(self,str_arg,message="Filename is not provided try again"):
@@ -8,14 +9,37 @@ class EmptyStringError(Exception):
 
 
 def read_data(filename):
-	ret=None
-	if type(filename) == str:
-		ret=pd.read_csv(filename)
+	ret = None
+	#print("==========")
+	#print(os.path.isfile(filename))
+	isFileExists = os.path.isfile(filename)
+	if (type(filename) == str):
+		#ret=pd.read_csv(filename)
+		ret=None
 	else:
-		raise TypeError("Your filename is not a string try again")
+		raise TypeError("File name is not str")
+	#print("does our file exist? ")
+	if ( len(filename) == 0):
+		raise EmptyStringError(filename)
+	if (isFileExists == False):
+		print("file non exist")
+		raise FileNotFoundError("File does not exist check file location")
+	if ( (isFileExists == True) and (type(filename) == str) and (len(filename)>0) ):
+		ret=pd.read_csv(filename)
+	#print(isFileExists)
+	#print("===========")
+	#ret=None
+	#print(len(filename))
 	return ret
 
 #ret=read_data("a.csv")
 #print(ret)
 #ret2=read_data(00)
 #print(ret2)
+#print("file length: ")
+#print(len(""))
+
+#ret3=read_data("t.csv")
+#print(ret3)
+#ret4=read_data("")
+#print(ret4)
